dd2208f7fafa1eb8d2c634e32942b6835287ef08
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-08-09 20:15:04 -0700

    reached MVP

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const uuid = require('uuid');
 const server = express();
 const users = require('./data');
+const e = require('express');
 
-const PORT = process.env.PORT || 8000;
+// Body Parser Middleware 
+server.use(express.json());
+server.use(express.urlencoded({ extended: false}));
 
 
-server.listen(PORT, () => {
-    console.log(`Server started on port${PORT}`);
-})
+
 
 
 
@@ -42,38 +44,73 @@ server.get('/api/users/:id', (req, res) => {
 
 
 
+
 // Creates a user using the information sent inside the `request 
-server.post('/api/user', (req, res) => {
-    res.json(user)
+server.post('/api/users', (req, res) => {
+    const newUser = {
+     id: uuid.v4(),
+     name: req.body.name,
+     email: req.body.email,
+     status: 'active'
+}
+     if(!newUser.name || !newUser.email) {
+         return res.status(400).json({msg: 'Please include a name and email '});
+         
+     } 
+     users.push(newUser);
+     res.json(users);
 });
 
-function createUser(data) {
-    const payload = {
-        id: String(users.length + 1),
-        ...data,
-    }
-    users.push(payload)
-        return payload
-}
 
+
+
+
+
+
+
+// put 
 /*  Updates the users information  */
-function updateUser(id, data) {
-    const index = users.findIndex(u => u.id === id)
-    users[index] = {
-        ...users[index],
-        ...data,
+server.put('/api/:id', (req, res) => {
+    const found = users.filter(user => user.id === parseInt(req.params.id));
+    
+    if (found.length > 0) {
+        const upUser =req.body;
+        users.forEach(user =>  {
+            if(user.id === parseInt(req.params.id)) {
+                user.name =updUser.name ?  upUser.name : user.name;
+                user.email = updUser.email ?  upUser.email : user.email;
+            
+            res.json({msg: 'user was updated, member' });
+            }
+        });  
+    } else {
+        res.status(400).json({msg: `No User with the id of ${req.params.id}`});
     }
-    return users[index]
-}
+});
 
 
+// deletes a User  
+server.delete('/api/:id', (req, res) => {
+    const found = users.filter(user => user.id === parseInt(req.params.id));
+    
+    if (found.length > 0) {
+        res.json({ msg: 'User deleted', });
+    } else {
+        res.status(400).json({msg: `No User with the id of ${req.params.id}`});
+    }
+   });
 
-/*  deletes a user  */
-function deleteUser(id) {
-    users = users.filter(u => u.id != id)
-}
+
 
 
 
 // when deployed add a process which means that I want to look at environment variables I want to look for the one called port OR look for 8000
+const PORT = process.env.PORT || 8000;
+
+
+server.listen(PORT, () => {
+    console.log(`Server started on port${PORT}`);
+})
+
+
 
